85144c110e9b51678a141ff0ef71fd8e022ded00
Graciána Adamcová <adamcova.grace@gmail.com>
2023-11-24 11:36:38 +0100

    component AboutPersonality

---
+++ src/components/AboutPersonality/index.jsx
@@ -0,0 +1,17 @@
+import { ButtonPrimary } from '../ButtonPrimary';
+import './style.css';
+
+export const AboutPersonality = () => {
+  return (
+    <section className="aboutPersonality">
+      <h2 className="aboutPersonality__heading">Co je to osobnost?</h2>
+      <p className="aboutPersonality__info">
+        Osobnost je pojem, který popisuje různé části lidské povahy. Zabývá se
+        lidským chováním, zahrnujícím psychické, emoční, společenské, tělesné a
+        duchovní aspekty, přičemž existuje tvrzení, že jedinec může mít i skryté
+        vlastnosti, kterým nerozumíme.
+      </p>
+      <ButtonPrimary>Zjistit více</ButtonPrimary>
+    </section>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,3 +1,4 @@
+import { AboutPersonality } from '../../components/AboutPersonality';
 import { ChooseTest } from '../../components/ChooseTest';
 import { Menu } from '../../components/Menu';
 import { PageIntro } from '../../components/PageIntro';
@@ -8,6 +9,7 @@ export const HomePage = () => {
       <Menu />
       <PageIntro />
       <ChooseTest />
+      <AboutPersonality />
     </main>
   );
 };
