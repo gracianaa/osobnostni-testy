11b98701da417c832a6250250e61587fdbbcf33d
Graciána Adamcová <adamcova.grace@gmail.com>
2023-11-28 23:14:59 +0100

    JSON structure updated with tests/questions/results and CardTest component updated with numberOfQuestions variable

--- src/components/CardTest/index.jsx
+++ src/components/CardTest/index.jsx
@@ -2,11 +2,13 @@ import { ButtonSecondary } from '../ButtonSecondary';
 import './style.css';
 
 export const CardTest = ({ test }) => {
+  const numberOfQuestions = test.questions ? test.questions.length : 0;
+
   return (
     <div className="cardTest">
       <h3 className="cardTest__title">{test.title}</h3>
       <p className="cardTest__info">
-        {test.time} minut | {test.numberOfQuestions} otázek
+        {test.minutes} minut | {numberOfQuestions} otázek
       </p>
       <div className="cardTest__image">
         <img src={test.img} alt={test.title} />
