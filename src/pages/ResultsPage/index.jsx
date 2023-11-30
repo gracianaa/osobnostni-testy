6d4aeb839c77ec274eee09eeffd2c1e5bf99240f
Graciána Adamcová <adamcova.grace@gmail.com>
2023-11-30 13:39:05 +0100

    ResultsPage created and styled

--- src/pages/ResultsPage/index.jsx
+++ src/pages/ResultsPage/index.jsx
@@ -1,7 +1,23 @@
+import tests from '../../../tests.json';
+import { ButtonSecondary } from '../../components/ButtonSecondary';
+import shareButton from './share.png';
+import './style.css';
+
 export const ResultsPage = () => {
   return (
-    <div>
-      <h4>Results</h4>
+    <div className="resultCard">
+      <h4 className="resultCard__title">{tests[0].results[0].result}</h4>
+      <p className="resultCard__description">
+        {tests[0].results[0].description}
+      </p>
+      <ButtonSecondary>
+        <div className="buttonShare">
+          <div className="buttonShare__image">
+            <img src={shareButton} alt="share" />
+          </div>
+          Sdílet s přáteli
+        </div>
+      </ButtonSecondary>
     </div>
   );
 };
