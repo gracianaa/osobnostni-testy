0ce6c1097cb97b5ec853fa2741197182297f2e69
Graciána Adamcová <adamcova.grace@gmail.com>
2023-12-01 11:42:17 +0100

    HowItWorks component updated with list-item style using CSS counters

--- src/components/HowItWorks/index.jsx
+++ src/components/HowItWorks/index.jsx
@@ -5,25 +5,24 @@ export const HowItWorks = () => {
     <section className="howItWorks" id="howItWorks">
       <h2 className="howItWorks__heading">Jak pracovat s testy?</h2>
       <div className="howItWorks__container">
-        <div className="howItWorks__flex">
-          <div className="howItWorks__number">1</div>
-          <p className="howItWorks__text">
-            U každého testu si nejprve přečti úvod a poté si projdi otázky.
-          </p>
-        </div>
-        <div className="howItWorks__flex">
-          <div className="howItWorks__number">2</div>
-          <p className="howItWorks__text">
-            Pamatuj, že neexistují správné a špatné odpovědi.
-          </p>
-        </div>
-        <div className="howItWorks__flex">
-          <div className="howItWorks__number">3</div>
-          <p className="howItWorks__text">
-            Nad otázkami nepřemýšlej, vyber odpověď, která k tobě nejvíc
-            promlouvá.
-          </p>
-        </div>
+        <ol className="howItWorks__list">
+          <li className="howItWorks__item">
+            <p className="howItWorks__text">
+              U každého testu si nejprve přečti úvod a poté si projdi otázky.
+            </p>
+          </li>
+          <li className="howItWorks__item">
+            <p className="howItWorks__text">
+              Pamatuj, že neexistují správné a špatné odpovědi.
+            </p>
+          </li>
+          <li className="howItWorks__item">
+            <p className="howItWorks__text">
+              Nad otázkami nepřemýšlej, vyber odpověď, která k tobě nejvíc
+              promlouvá.
+            </p>
+          </li>
+        </ol>
       </div>
     </section>
   );
