29ec457550cdff16afded046a641438b5b16d2ea
Graciána Adamcová <adamcova.grace@gmail.com>
2023-12-02 19:18:20 +0100

    AboutPersonality, AnswerList updated style and ButtonPrimary adjusted

--- src/components/ButtonPrimary/index.jsx
+++ src/components/ButtonPrimary/index.jsx
@@ -1,9 +1,5 @@
 import './style.css';
 
 export const ButtonPrimary = ({ children }) => {
-  return (
-    <div className="button__container">
-      <button className="button button--primary">{children}</button>
-    </div>
-  );
+  return <button className="button button--primary">{children}</button>;
 };
