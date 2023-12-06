c88d16293c00f5cfa934e833f997b3276c0230cf
Karolína Mukařovská <k.mukarovska01@gmail.com>
2023-12-06 10:39:40 +0100

    About personality section styled to be responsive

--- src/components/AboutPersonality/index.jsx
+++ src/components/AboutPersonality/index.jsx
@@ -5,18 +5,28 @@ import ideaMan from './idea.png';
 export const AboutPersonality = () => {
   return (
     <section className="aboutPersonality" id="aboutPersonality">
-      <h2 className="aboutPersonality__heading">Co je to osobnost?</h2>
-      <p className="aboutPersonality__info">
-        Osobnost je pojem, který popisuje různé části lidské povahy. Zabývá se
-        lidským chováním, zahrnujícím psychické, emoční, společenské, tělesné a
-        duchovní aspekty, přičemž existuje tvrzení, že jedinec může mít i skryté
-        vlastnosti, kterým nerozumíme.
-      </p>
+      <div className="aboutPersonality__text">
+        <h2 className="aboutPersonality__heading">Co je to osobnost?</h2>
+        <p className="aboutPersonality__info">
+          Osobnost je pojem, který popisuje různé části lidské povahy. Zabývá se
+          lidským chováním, zahrnujícím psychické, emoční, společenské, tělesné
+          a duchovní aspekty, přičemž existuje tvrzení, že jedinec může mít i
+          skryté vlastnosti, kterým nerozumíme.
+        </p>
+        <p className="aboutPersonality__infoDesktop">
+          Lidské bytosti jsou složitá stvoření, ale do všeho kolem sebe se snaží
+          vnést pořádek, včetně toho, že sami definují ostatní. Psychologové se
+          neustále snaží lidi definovat a kategorizovat. Otázky, proč se lidé
+          chovají odlišně a proč jsme každý jedinečný, jsou předmětem neustálých
+          výzkumů. Konkrétně se jedná o porovnání lidí se sklony k patologickému
+          chování a lidí bez těchto sklonů. Kde bychom byli bez krimi thrillerů,
+          které zkoumají psychologické chování zločinců?
+        </p>
 
-      <Button to="/personality" type={'primary'}>
-        Zjistit více
-      </Button>
-
+        <Button to="/personality" type={'primary'}>
+          Zjistit více
+        </Button>
+      </div>
       <div className="aboutPersonality__img">
         <img src={ideaMan} alt="Man with an idea" />
       </div>
