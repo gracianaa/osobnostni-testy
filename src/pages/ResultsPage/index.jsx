11e2b3f916928feb570b610e167ecb1b6eddc3b7
Graciána Adamcová <adamcova.grace@gmail.com>
2023-12-06 23:01:01 +0100

    save PDF button in ResultPage

--- src/pages/ResultsPage/index.jsx
+++ src/pages/ResultsPage/index.jsx
@@ -1,9 +1,24 @@
 import { Button } from '../../components/Button';
-import shareButton from './share.png';
+import pdfButton from './pdf_v2.png';
 import './style.css';
 import { useOutletContext, useParams } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import { useRef } from 'react';
 
 export const ResultsPage = () => {
+  const refPDF = useRef();
+
+  const downloadPDF = () => {
+    const input = refPDF.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      pdf.addImage(imgData, 'JPEG', 10, 5, 200, 80);
+      pdf.save('personality.pdf');
+    });
+  };
+
   const { score } = useParams();
   const test = useOutletContext();
   const results = test.results;
@@ -21,7 +36,7 @@ export const ResultsPage = () => {
   return (
     <div className="content">
       <div className="resultCard">
-        <div className="resultCard__content">
+        <div className="resultCard__content" ref={refPDF}>
           <div className="resultCard__text">
             <h4 className="resultCard__title">{bestResult.result}</h4>
             <p className="resultCard__description">{bestResult.description}</p>
@@ -30,12 +45,12 @@ export const ResultsPage = () => {
             <img src={bestResult.image} alt={bestResult.image} />
           </div>
         </div>
-        <Button type={'secondary'}>
+        <Button onClick={downloadPDF} type={'secondary'}>
           <div className="buttonShare">
             <div className="buttonShare__image">
-              <img src={shareButton} alt="share" />
+              <img src={pdfButton} alt="share" />
             </div>
-            Sdílet s přáteli
+            Uložit jako PDF
           </div>
         </Button>
       </div>
