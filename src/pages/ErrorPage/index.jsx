07c7da9ff70b738c36c5327bfa5ba7a6712cb093
Karolína Mukařovská <k.mukarovska01@gmail.com>
2023-12-07 15:24:34 +0100

    eror page styles

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,7 +1,17 @@
+import './style.css';
+import eror from './eror.png';
+import { Button } from '../../components/Button';
+
 export const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 ERROR</h1>
+    <div className="erorPage__content">
+      <h1 className="erorPage__heading">Ups, tudy cesta nevede...</h1>
+      <Button to={'/'} type={'secondary'}>
+        Zpět na hlavní stránku
+      </Button>
+      <div className="erorPage__img">
+        <img src={eror} alt="eror image" />
+      </div>
     </div>
   );
 };
